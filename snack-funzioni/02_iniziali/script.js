99b1d616664db021973f5d538e4157199ab3ad72
Rossella <rossellataverni02@gmail.com>
2025-03-24 16:56:35 +0100

    [UPD] Update second exercise completed.

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function firstLetter(word){
+    // Con Array.isArray andiamo a verificare che l'argomento passato alla funzione sia effettivamente un array
+    if(Array.isArray(word)){
+        // Con .map() applichiamo la logica a tutte le parole nell'array
+        // restituendo un nuovo array con le iniziali in maiuscolo
+        const letter = word.map(word => word.charAt(0).toUpperCase());
+        return letter;
+    }    
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+firstLetter(names);
+console.log(firstLetter(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
