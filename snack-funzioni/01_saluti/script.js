a8850ab53ca825eb1e59bdc15621988163c538b5
Rossella <rossellataverni02@gmail.com>
2025-03-24 16:26:52 +0100

    [UPD] First exercise update completed.

--- snack-funzioni/01_saluti/script.js
+++ snack-funzioni/01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto(stringa){
+    console.log('Ciao ' + stringa)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+saluto(userName);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
